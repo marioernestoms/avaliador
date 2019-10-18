ff2958532290b42a1a36d26050af97d2673509a7
Mario Ernesto <marioernesto.ms@gmail.com>
2019-10-18 11:24:58 -0300

    mover css de nova avaliacao para arquivo sass

--- themes/avaliador/includes/modal/modal-nova-avaliacao.php
+++ themes/avaliador/includes/modal/modal-nova-avaliacao.php
@@ -1,33 +1,3 @@
-<style>
-.acf-field-5da86588a6c5a {
-    height: 300px;
-    overflow: hidden;
-    overflow-y: scroll;
-}
-
-#acf-field_5da86588a6c5a {
-    border: 0;
-    padding: 10px 0;
-}
-
-.selection {
-    border: 1px solid #ced4da;
-    border-radius: .3rem;
-    display: flex;
-    margin-top: 11px;
-    width: 100%;
-}
-
-.choices {
-    border-right: 1px solid rgba(0,2,0,.2);
-    width: 50%;
-}
-
-.values {
-    width: 50%;
-}
-</style>
-
 <div class="modal fade text-left" id="nova-avaliacao" role="dialog" tabindex="-1" aria-labelledby="nova-avaliacao" aria-hidden="true" style="display: none;">
 	<div class="modal-dialog">
 		<div class="modal-content">
